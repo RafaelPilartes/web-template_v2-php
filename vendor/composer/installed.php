dea6d93d1b0c572bf0c984b7ed7f9ce947ee1099
Rafael Pilartes <rafaelpilartes.rlps@gmail.com>
2023-12-18 17:26:20 +0200

    raw - vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'alexandrexandecar/simple-routes',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexandrexandecar/simple-routes' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'dev_requirement' => false,
+        ),
+    ),
+);
